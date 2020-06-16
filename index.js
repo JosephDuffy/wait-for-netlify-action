c302c12822670c8a8d80bf160b2ac154a66ba8e7
Lu Nelson <lunelson@gmail.com>
2020-06-16 13:31:56 +0200

    change netlify domain to .app; reduce poll frequency

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const github = require("@actions/github");
 const axios = require("axios");
 
 const waitForUrl = async (url, MAX_TIMEOUT) => {
-  const iterations = MAX_TIMEOUT / 2;
+  const iterations = MAX_TIMEOUT / 3;
   for (let i = 0; i < iterations; i++) {
     try {
       await axios.get(url);
       return;
     } catch (e) {
       console.log("Url unavailable, retrying...");
-      await new Promise(r => setTimeout(r, 2000));
+      await new Promise(r => setTimeout(r, 3000));
     }
   }
   core.setFailed(`Timeout reached: Unable to connect to ${url}`);
@@ -24,7 +24,7 @@ const run = async () => {
     if (!siteName) {
       core.setFailed("Required field `site_name` was not provided");
     }
-    const url = `https://${commit}--${siteName}.netlify.com`;
+    const url = `https://${commit}--${siteName}.netlify.app`;
     core.setOutput("url", url);
     console.log(`Waiting for a 200 from: ${url}`);
     await waitForUrl(url, MAX_TIMEOUT);
